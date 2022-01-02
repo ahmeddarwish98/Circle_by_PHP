88db3ca6cd999e4cf12046bcd3787a58211e9997
AHMED Sobhy <ahmed.sobhy1310@ci.menofia.edu.eg>
2022-01-02 22:09:18 +0200

    Initial commit

---
+++ circle.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+
+$a=10;
+
+$area = (22/7)*$a*$a;                          /////// مساحه /////////
+echo("The Area = ".$area);
+echo("<br>");
+$circumference = 2*(22/7)*$a;
+echo("The circumference = ".$circumference);  //////// محيط ///////
+
+
+?>
+
+</body>
+</html>
